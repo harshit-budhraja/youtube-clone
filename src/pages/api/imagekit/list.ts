66ebc8fae7f463beb5e54e9e6673838426d3f865
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2025-01-23 19:31:56 +0530

    chore: add placeholder thumbnail for starters

--- src/pages/api/imagekit/list.ts
+++ src/pages/api/imagekit/list.ts
@@ -22,15 +22,17 @@ export default async function handler(
   // To send object containing: id, thumbnailUrl using imagekit.url(path/ik-thumbnail.jpg)
   // and queryParameters: { updatedAt: new Date(file.updatedAt).getTime().toString() }
   // url, title, description, duration, createdAt
+  // TODO: Update thumbnailUrl to use imagekit.url
   res.status(200).json(
     files.map((file) => ({
       id: file.fileId,
-      thumbnailUrl: imagekit.url({
-        path: `${file.filePath}/ik-thumbnail.jpg`,
-        queryParameters: {
-          updatedAt: new Date(file.updatedAt).getTime().toString(),
-        },
-      }),
+      thumbnailUrl: "https://placehold.co/800X400.webp?text=Video%20thumbnail%20goes%20here",
+      // thumbnailUrl: imagekit.url({
+      //   path: `${file.filePath}/ik-thumbnail.jpg`,
+      //   queryParameters: {
+      //     updatedAt: new Date(file.updatedAt).getTime().toString(),
+      //   },
+      // }),
       title: file.customMetadata?.Title ?? file.name,
       description: file.customMetadata?.Description ?? file.name,
       duration: (file as any).duration ?? 0,
